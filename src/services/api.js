c3314c00fa201e4d86ee091ad041a81ae7dedaba
Pratyush <pratyushbhardwaj95@gmail.com>
2026-01-03 16:58:20 +0530

    update baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL:  'http://localhost:5000/api',
+   baseURL: 'https://contact-backend-z2ga.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
